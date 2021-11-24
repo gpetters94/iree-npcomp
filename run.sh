e0443f0be9e4e69213545276e661995b90b7c063
George Petterson <gpetters@protonmail.com>
2021-11-24 14:04:15 -0500

    Update scripts

--- run.sh
+++ run.sh
@@ -19,32 +19,32 @@ export PYTHONPATH="$PYTHONPATHONE:$PYTHONPATH"
 
 for MODEL in models/*.py; do
   echo "Running $MODEL"
-  python3 "$MODEL" ||
-  rm -rf out &&
-  mkdir -p out &&
-  external/iree/build/compiler-api/python_package/iree/compiler/_mlir_libs/ireec \
-    --iree-input-type=none \
-    --iree-vm-bytecode-module-output-format=flatbuffer-binary \
-    --iree-hal-target-backends=dylib-llvm-aot \
-    --iree-mlir-to-vm-bytecode-module \
-    --iree-llvm-embedded-linker-path=external/iree/build/compiler-api/python_package/iree/compiler/_mlir_libs/iree-lld \
-    --mlir-print-debuginfo \
-    --mlir-print-op-on-diagnostic=false \
-    --print-ir-before-all \
-    --print-ir-after-failure \
-    uncompiled.mlir 2>out.mlir ||
-  csplit out.mlir -n 4 -f out/pass_ '/\/\/\ -----\/\//' '{*}' &&
-  rm out.mlir &&
-  gdb --args \
-    external/iree/build/compiler-api/python_package/iree/compiler/_mlir_libs/ireec \
-      --iree-input-type=none \
-      --iree-vm-bytecode-module-output-format=flatbuffer-binary \
-      --iree-hal-target-backends=dylib-llvm-aot \
-      --iree-mlir-to-vm-bytecode-module \
-      --iree-llvm-embedded-linker-path=external/iree/build/compiler-api/python_package/iree/compiler/_mlir_libs/iree-lld \
-      --mlir-print-debuginfo \
-      --mlir-print-op-on-diagnostic=false \
-      uncompiled.mlir
+  python3 "$MODEL" #||
+#  rm -rf out &&
+#  mkdir -p out &&
+#  external/iree/build/compiler-api/python_package/iree/compiler/_mlir_libs/ireec \
+#    --iree-input-type=none \
+#    --iree-vm-bytecode-module-output-format=flatbuffer-binary \
+#    --iree-hal-target-backends=dylib-llvm-aot \
+#    --iree-mlir-to-vm-bytecode-module \
+#    --iree-llvm-embedded-linker-path=external/iree/build/compiler-api/python_package/iree/compiler/_mlir_libs/iree-lld \
+#    --mlir-print-debuginfo \
+#    --mlir-print-op-on-diagnostic=false \
+#    --print-ir-before-all \
+#    --print-ir-after-failure \
+#    uncompiled.mlir 2>out.mlir ||
+#  csplit out.mlir -n 4 -f out/pass_ '/\/\/\ -----\/\//' '{*}' &&
+#  rm out.mlir &&
+#  gdb --args \
+#    external/iree/build/compiler-api/python_package/iree/compiler/_mlir_libs/ireec \
+#      --iree-input-type=none \
+#      --iree-vm-bytecode-module-output-format=flatbuffer-binary \
+#      --iree-hal-target-backends=dylib-llvm-aot \
+#      --iree-mlir-to-vm-bytecode-module \
+#      --iree-llvm-embedded-linker-path=external/iree/build/compiler-api/python_package/iree/compiler/_mlir_libs/iree-lld \
+#      --mlir-print-debuginfo \
+#      --mlir-print-op-on-diagnostic=false \
+#      uncompiled.mlir
 done
 
 deactivate
