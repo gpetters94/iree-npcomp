4d735bd63f56ab6ddf4be3d137df63f1f0859593
George Petterson <gpetters@protonmail.com>
2021-12-02 20:31:53 -0500

    Cleanup

--- e2e.sh
+++ e2e.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
 
 source venv/bin/activate
-
-python3 -m pip install transformers fairseq fvcore sacremoses subword-nmt > /dev/null
-SERIAL="$PWD/serialized_tests"
-rm -rf "$SERIAL"
-mkdir -p "$SERIAL/tests"
-pushd external/torch-mlir
-cmake --build build --target check-torch-mlir
-build_tools/write_env_file.sh
-export PYTHONPATH=${PYTHONPATH-}
-source ".env"
-python3 -m build_tools.torchscript_e2e_heavydep_tests.main --output_dir="$SERIAL/tests" || exit
-unset PYTHONPATH
-popd
+JOBCOUNT="$(($(nproc) + 2))"
 
 pushd external/torch-mlir
 [[ -e .env ]] || build_tools/write_env_file.sh
 source .env
-cmake --build build -j 3
+cmake --build build -j "$JOBCOUNT"
 popd
 
 PYTHONPATHONE="$PYTHONPATH"
@@ -26,7 +14,7 @@ unset PYTHONPATH
 
 pushd external/iree
 source .env
-cmake --build build -j 3
+cmake --build build -j "$JOBCOUNT"
 popd
 
 export PYTHONPATH="$PYTHONPATHONE:$PYTHONPATH"
@@ -41,8 +29,6 @@ cd external/torch-mlir
 # Ensure PYTHONPATH is set for export to child processes, even if empty.
 export PYTHONPATH=${PYTHONPATH-}
 
-# python -m e2e_testing.torchscript.main --serialized-test-dir "../../serialized_tests" --filter=MiniLMSequenceClassification_basic -v
-python -m e2e_testing.torchscript.main --serialized-test-dir "../../serialized_tests" --filter=MiniLMSequenceClassification_basic -v -c external --external-config "../../models/torchscript_e2e_config.py"
-# python -m e2e_testing.torchscript.main -v -c external --external-config "../../models/torchscript_e2e_config.py" "$@"
+python -m e2e_testing.torchscript.main -v -c external --external-config "../../models/torchscript_e2e_config.py" "$@"
 
 deactivate
--- run.sh
+++ run.sh
@@ -1,52 +1,47 @@
 #!/bin/bash
 
-SERIAL="$PWD/serialized_tests"
-pushd external/torch-mlir
-[[ -e "$SERIAL" ]] || build_tools/torchscript_e2e_heavydep_tests/generate_serialized_tests.sh "$SERIAL/venv" "$SERIAL/tests"
-popd
+set -euo pipefail
+JOBCOUNT="$(($(nproc) + 2))"
 
 source venv/bin/activate
 
+python3 -m pip install transformers fairseq fvcore sacremoses subword-nmt > /dev/null
+SERIAL="$PWD/serialized_tests"
+rm -rf "$SERIAL"
+mkdir -p "$SERIAL/tests"
 pushd external/torch-mlir
+cmake --build build --target check-torch-mlir
 build_tools/write_env_file.sh
+export PYTHONPATH=${PYTHONPATH-}
+source ".env"
+python3 -m build_tools.torchscript_e2e_heavydep_tests.main --output_dir="$SERIAL/tests" || exit
+unset PYTHONPATH
+popd
+
+pushd external/torch-mlir
+[[ -e .env ]] || build_tools/write_env_file.sh
 source .env
-cmake --build build -j 3 || exit
+cmake --build build -j "$JOBCOUNT" || exit
 popd
 
 PYTHONPATHONE="$PYTHONPATH"
 
 pushd external/iree
 source .env
-cmake --build build -j 3 || exit
+cmake --build build -j "$JOBCOUNT" || exit
 popd
 
-export PYTHONPATH="$PYTHONPATH:$PYTHONPATHONE"
+PYTHONPATH="$PYTHONPATH:$PYTHONPATHONE"
+export PYTHONPATH=${PYTHONPATH-}
 
-python3 models/resnet18.py # ||
-# rm -rf out &&
-# mkdir -p out &&
-# external/iree/build/compiler-api/python_package/iree/compiler/_mlir_libs/ireec \
-#   --iree-input-type=none \
-#   --iree-vm-bytecode-module-output-format=flatbuffer-binary \
-#   --iree-hal-target-backends=dylib-llvm-aot \
-#   --iree-mlir-to-vm-bytecode-module \
-#   --iree-llvm-embedded-linker-path=external/iree/build/compiler-api/python_package/iree/compiler/_mlir_libs/iree-lld \
-#   --mlir-print-debuginfo \
-#   --mlir-print-op-on-diagnostic=false \
-#   --print-ir-before-all \
-#   --print-ir-after-failure \
-#   uncompiled.mlir 2>out.mlir ||
-# csplit out.mlir -n 4 -f out/pass_ '/\/\/\ -----\/\//' '{*}' &&
-# rm out.mlir &&
-# gdb --args \
-#   external/iree/build/compiler-api/python_package/iree/compiler/_mlir_libs/ireec \
-#     --iree-input-type=none \
-#     --iree-vm-bytecode-module-output-format=flatbuffer-binary \
-#     --iree-hal-target-backends=dylib-llvm-aot \
-#     --iree-mlir-to-vm-bytecode-module \
-#     --iree-llvm-embedded-linker-path=external/iree/build/compiler-api/python_package/iree/compiler/_mlir_libs/iree-lld \
-#     --mlir-print-debuginfo \
-#     --mlir-print-op-on-diagnostic=false \
-#     uncompiled.mlir
+# Resnet
+echo "===Running Resnet==="
+python3 models/resnet18.py
+
+# BERT
+pushd external/torch-mlir
+echo "===Running BERT==="
+python -m e2e_testing.torchscript.main --serialized-test-dir "../../serialized_tests" --filter=MiniLMSequenceClassification_basic -v -c external --external-config "../../models/torchscript_e2e_config.py"
+popd
 
 deactivate
